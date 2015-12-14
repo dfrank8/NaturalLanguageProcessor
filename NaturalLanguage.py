5d8b81366b3c16ba04ece9141f9aabfc0fd27043
Douglas Franklin <douglasfranklin@Douglass-MacBook-Air.local>
2015-12-14 14:06:51 -0500

    Done! Re-did the entire analysis process and it works WAY better. The user input feature was added, and the analyzed posts get added to the objects after being analyzed

--- NaturalLanguage.py
+++ NaturalLanguage.py
@@ -14,6 +14,7 @@ import sys
 import tfidf
 import json
 import terms
+import math
 import argparse
 import fileParser
 from argparse import RawTextHelpFormatter
@@ -64,9 +65,9 @@ def analyzeBlogs(blogList): # Analyze blog with tfidf, and other word analysis.
                 if word in terms.religiousTerms():
                     religionCount+=1
                 sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True) # sort the words
-        for word, score in sorted_words[0:5]:
+        for word, score in sorted_words[0:10]:
             print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
-            outputWordsArr.append((word, round(score, 5)))
+            outputWordsArr.append((word, round(score, 10)))
         print("---------------------------------------------------------")
     # Gathering the density scores of each of these defined features, and creating the returning data type
     analysisOutputs = AnalysisObject(namesCount/wordCount,religionCount/wordCount,weaponryCount/wordCount,governmentCount/wordCount,outputWordsArr)
@@ -83,46 +84,54 @@ def applyWeights(features, weightedFeatures): # Apply weights to the scores in t
     return weightedFeatures
 
 def analyzeNewBlog(blog, goodBlogList, badBlogList, features):
-    # dictionaries are mutable, so this method will update features
-    # run analyze blogs as if the new blog was in both, whichever feature is LESS significantly changed, it probably belongs in that one
-    varianceScores = {"good": 0.0, "bad": 0.0} # The new entry is analyzed as if it was amongst both parties, which ever one it had the bigger effect on changing, it belongs to the other. 
-    goodBlogList.append(tb(blog))
-    badBlogList.append(tb(blog))
-
-    featuresAfter = {"good":{"words": [], "names": 0.0, "religion": 0.0, "weaponry": 0.0, "government": 0.0}, "bad": {"words": [], "names": 0.0, "religion": 0.0, "weaponry": 0.0, "government": 0.0}}
-    analysisResults = analyzeBlogs(goodBlogList)
-    featuresAfter["good"]["words"], featuresAfter["good"]["names"], featuresAfter["good"]["religion"], featuresAfter["good"]["weaponry"], featuresAfter["good"]["government"] = analysisResults.outputsWordsArray, analysisResults.namesScore, analysisResults.religionScore, analysisResults.weaponryScore, analysisResults.governmentScore
-    analysisResults = analyzeBlogs(badBlogList)
-    featuresAfter["bad"]["words"], featuresAfter["bad"]["names"], featuresAfter["bad"]["religion"], featuresAfter["bad"]["weaponry"], featuresAfter["bad"]["government"] = analysisResults.outputsWordsArray, analysisResults.namesScore, analysisResults.religionScore, analysisResults.weaponryScore, analysisResults.governmentScore
-
-    for upperKey in featuresAfter:
-        total = 0.0
-        featureCount = 0 
-        for lowerKey in featuresAfter[upperKey]:
+    # Get word densities of the new blog
+    namesCount, religionCount, weaponryCount, governmentCount, wordCount = 0, 0, 0, 0, 0
+    for word in tb(blog):
+        wordCount += 1
+        if word in terms.governmentTerms(): # increment count based on content to find word densities. 
+            governmentCount += 1
+        if word in terms.weaponsTerms():
+            weaponryCount += 1
+        if word in terms.femaleNames() or word in terms.maleNames():
+            namesCount += 1
+        if word in terms.religiousTerms():
+            religionCount += 1
+    analysisOutputs = AnalysisObject(namesCount/wordCount,religionCount/wordCount,weaponryCount/wordCount,governmentCount/wordCount, None)
+   
+   # Compare to the analyzed ones.
+    scores = {"good": 0.0, "bad": 0.0}
+    for upperKey in features:
+        print ("\nComparing this blog to " + upperKey.upper() + " blogs:\n")
+        for lowerKey in features[upperKey]:
             if lowerKey == "words":
-                if featuresAfter[upperKey][lowerKey][-5:-1] in features[upperKey][lowerKey]: # This means that the new words were apparent in previous posts
-                    varianceScores[upperKey] += 1/len(features[upperKey][lowerKey])
-            else:
-                varianceScores[upperKey] += featuresAfter[upperKey][lowerKey]/features[upperKey][lowerKey]
-            featureCount += 1
-        varianceScores[upperKey] /= featureCount
-
-    pdb.set_trace()
-    if varianceScores["good"] > varianceScores["bad"]:
-        del badBlogList[-1]
-        print("This blog has some aspects commonly seen in other writings made by bad people...")
+                for word in features[upperKey][lowerKey]:
+                    if word[0] not in terms.stopWords():
+                        if word[0] in blog:
+                            print ("Word found in " + upperKey + " blog: " + word[0])
+                            scores[upperKey] += word[1] * 100 # If a word is found, update the score relative to its TFIDF score. 
+            elif lowerKey == "religion": # This next section is to compare the density of a term of the new blog compared to the density of that term in the analyzed blogs. 
+                scores[upperKey] -= abs(features[upperKey][lowerKey] - analysisOutputs.religionScore)
+                print ("Religion variance: " + str(features[upperKey][lowerKey] - analysisOutputs.religionScore))
+            elif lowerKey == "government":
+                scores[upperKey] -= abs(features[upperKey][lowerKey] - analysisOutputs.governmentScore)
+                print ("Government variance: " + str(abs(features[upperKey][lowerKey] - analysisOutputs.governmentScore)))
+            elif lowerKey == "weaponry":
+                scores[upperKey] -= abs(features[upperKey][lowerKey] - analysisOutputs.weaponryScore)
+                print ("Weaponry variance: " + str(abs(features[upperKey][lowerKey] - analysisOutputs.weaponryScore)))
+            elif lowerKey == "names":
+                scores[upperKey] -= abs(features[upperKey][lowerKey] - analysisOutputs.namesScore)
+                print ("Names variance: " + str(abs(features[upperKey][lowerKey] - analysisOutputs.namesScore)))
+    print ("\nFinal Scores:\n" + "Bad: " + str(scores["bad"]) + "\nGood: " + str(scores["good"]) + "\n")
+    if abs(scores["good"] - scores["bad"]) < .5:
+        print ("This post does not trend towards 'good; or 'bad'.")
     else:
-        del goodBlogList[-1]
-        print("This blog is likely written by person who poses no threat to others")
-
-    # Clean up some memory space. 
-    del featuresAfter
-    del analysisResults
-
-
-
-# def updateFeatures(feature, newFeatures):
-    # update good and bad features depending on the decision by the alogrithm of which class it falls under.
+        if scores["good"] > scores["bad"]:
+            print ("This post has been marked as 'good'.")
+            goodBlogList.append(tb(blog)) # Add term to the blog list. If this program were running constantly, it would be included in the next baes analysis.
+        else: 
+            print ("This post has been flagged as 'bad'.")
+            badBlogList.append(tb(blog))
+    print ("\n---------------------------------------")
 
 def buildNewBlog(blogFile = None, blogAuthor = "", blogTitle = "", blogText = None): # Returns an analyzable object
     newBlog = None
@@ -156,63 +165,47 @@ def main():
     newBlogText = args.blog
     newBlogAuthor = args.author
     newBlogTitle = args.title
+    go = True
+    while(go):    
+        # The below object is a dictionary of 2 dictionaries, good and bad features, and their relevant metadata. 
+        # count is the number of times blogs have been passed through. This is necessary for updates.
+        features = {"good":{"count": 0, "words": [], "names": 0.0, "religion": 0.0, "weaponry": 0.0, "government": 0.0}, "bad": {"count": 0, "words": [], "names": 0.0, "religion": 0.0, "weaponry": 0.0, "government": 0.0}}
+        
+        json_data = importJSON("Writings/writings.json") # get JSON data, creating a dictionary-like object 
+        
+        # Declaring lists of writings
+        badBlogList = []
+        goodBlogList = []
+        
+        # Analyze the current data in the JSON file. 
+        for blog in json_data["writings"]["bad"]:
+        	badBlogList.append(tb(blog["post"]))
+        for blog in json_data["writings"]["good"]:
+        	goodBlogList.append(tb(blog["post"]))
+        analysisResults = analyzeBlogs(badBlogList)
+        features["bad"]["count"], features["bad"]["words"], features["bad"]["names"], features["bad"]["religion"], features["bad"]["weaponry"], features["bad"]["government"] = len(badBlogList), analysisResults.outputsWordsArray, analysisResults.namesScore, analysisResults.religionScore, analysisResults.weaponryScore, analysisResults.governmentScore
+        analysisResults = analyzeBlogs(goodBlogList)
+        features["good"]["count"], features["good"]["words"], features["good"]["names"], features["good"]["religion"], features["good"]["weaponry"], features["good"]["government"] = len(goodBlogList), analysisResults.outputsWordsArray, analysisResults.namesScore, analysisResults.religionScore, analysisResults.weaponryScore, analysisResults.governmentScore
     
-    # The below object is a dictionary of 2 dictionaries, good and bad features, and their relevant metadata. 
-    # count is the number of times blogs have been passed through. This is necessary for updates.
-    features = {"good":{"count": 0, "words": [], "names": 0.0, "religion": 0.0, "weaponry": 0.0, "government": 0.0}, "bad": {"count": 0, "words": [], "names": 0.0, "religion": 0.0, "weaponry": 0.0, "government": 0.0}}
-    
-    # Weighted features are kept in a separate objects since they are highly readable and more interesting to the user. 
-    # The metadata from features is applied to weighteFeatures after being multiplied by their respective weights. 
-    # Notice that words are ommitted from this object since the words cannot hold weight (since they are words, not numbers)
-    # weightedFeatures = {"good":{"names": 0.0, "religion": 0.0, "weaponry": 0.0, "government": 0.0}, "bad": {"names": 0.0, "religion": 0.0, "weaponry": 0.0, "government": 0.0}}
-    
-    json_data = importJSON("Writings/writings.json") # get JSON data, creating a dictionary-like object 
-    
-    # Declaring lists of writings
-    badBlogList = []
-    goodBlogList = []
-    
-    # Analyze the current data in the JSON file. 
-    for blog in json_data["writings"]["bad"]:
-    	badBlogList.append(tb(blog["post"]))
-    for blog in json_data["writings"]["good"]:
-    	goodBlogList.append(tb(blog["post"]))
+        print("Current writings in database have been analyzed... \nRunning comparisons against provided writing...\n ----------------------------")
+        
+        newBlog = None
+        # Analyze new file
+        if newBlogFile is not None:
+            newBlog = buildNewBlog(newBlogFile)
+        elif newBlogText is not None:
+            newBlog = buildNewBlog(None, newBlogAuthor, newBlogTitle, newBlogText)
+        
+        if newBlog is not None:
+            tempFeatures = {"words": [], "names": 0.0, "religion": 0.0, "weaponry": 0.0, "government": 0.0}
+            analyzeNewBlog(newBlog.post, goodBlogList, badBlogList, features)
 
+        print ("Please enter another file for analysis. or 'quit' to quit.\n")
+        newBlogFile = input('File path: ')
+        if newBlogFile == "quit" or newBlogFile == "Quit" or newBlogFile == "q":
+            go = False
     
-    pdb.set_trace()
-    analysisResults = analyzeBlogs(badBlogList)
-    features["bad"]["count"], features["bad"]["words"], features["bad"]["names"], features["bad"]["religion"], features["bad"]["weaponry"], features["bad"]["government"] = len(badBlogList), analysisResults.outputsWordsArray, analysisResults.namesScore, analysisResults.religionScore, analysisResults.weaponryScore, analysisResults.governmentScore
-    analysisResults = analyzeBlogs(goodBlogList)
-    features["good"]["count"], features["good"]["words"], features["good"]["names"], features["good"]["religion"], features["good"]["weaponry"], features["good"]["government"] = len(goodBlogList), analysisResults.outputsWordsArray, analysisResults.namesScore, analysisResults.religionScore, analysisResults.weaponryScore, analysisResults.governmentScore
-    
-    # pdb.set_trace()
-    # weightedFeatures = applyWeights(features, weightedFeatures)
-    # goodScore, badScore = 0.0, 0.0
-
-    print("Current writings in database have been analyzed... \nRunning comparisons against provided writing...\n ----------------------------")
-    
-    newBlog = None
-    # Analyze new file
-    if newBlogFile is not None:
-        newBlog = buildNewBlog(newBlogFile)
-    elif newBlogText is not None:
-        newBlog = buildNewBlog(None, newBlogAuthor, newBlogTitle, newBlogText)
-    
-    if newBlog is not None:
-        tempFeatures = {"words": [], "names": 0.0, "religion": 0.0, "weaponry": 0.0, "government": 0.0}
-      
-        analyzeNewBlog(newBlog.post, goodBlogList, badBlogList, features)
-        pdb.set_trace()
-        # analyze, decide good or bad, then append to json_object
-
-    # Update and add new data to JSON object
-
-    # Wait
-
-    # Next, check apply weights for the other word types mentioned in the report. 
-    # Generate feature weights based on if they appear in the most common words, 
-    # relative to if they just appear normally
-    
-    pdb.set_trace() # End, check on debug. 
+    print("Closing program...")
+    # pdb.set_trace() # End, check on debug. 
 if __name__ == "__main__": main()
 
